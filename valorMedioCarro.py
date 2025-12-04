c95890a29cb91b0556f90101515b6597c765d6cf
EmilyCutrim <emilykerendev@gmail.com>
2025-12-04 20:28:29 -0300

    valor médio de um carro ao longo de três anos usando if e else

---
+++ valorMedioCarro.py
@@ -0,0 +1,16 @@
+#Escreva um programa que leia valores médios de preços de um modelo de carro por 3 anos 
+# consecutivos e exiba o valor mais alto e mais baixo entre esses três anos.
+
+texto_1= float(input("Preço do carro em 2022: "))
+texto_2 = float(input("Preço do carro em 2023: "))
+texto_3 = float(input("Preço do carro em 2024: "))
+
+if texto_1 >= texto_2 and texto_2 >= texto_3:
+  print (f'o carro de 2022, no valor de R$ {texto_1} é o maior valor médio entres os três anos, sendo o de 2024, no valor de R$ {texto_3} o mais barato')
+elif texto_2 >= texto_3 and texto_2 >= texto_1:
+  print (f'o carro de 2023, no valor de R$ {texto_2} é o maior valor médio entres os três anos, sendo o de 2022, no valor de R$ {texto_1} o mais barato')
+elif texto_3 >= texto_1 and texto_1 >= texto_2:   
+  print (f'o carro de 2024, no valor de R$ {texto_3} é o maior valor médio entres os três anos, sendo o de 2023, no valor de R$ {texto_2} o mais barato')
+elif texto_2 >= texto_1 and texto_3 >= texto_2:   
+  print (f'o carro de 2024, no valor de R$ {texto_3} é o maior valor médio entres os três anos, sendo o de 2023, no valor de R$ {texto_2} o mais barato')
+  
