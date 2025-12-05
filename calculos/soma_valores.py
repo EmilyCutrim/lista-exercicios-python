1933529f53a18217893c84ddf8367b2e30cd7369
EmilyCutrim <emilykerendev@gmail.com>
2025-12-04 22:28:27 -0300

    feat(code): Adiciona script para cálculo da soma de três valores

---
+++ calculos/soma_valores.py
@@ -0,0 +1,6 @@
+primeiro_valor = int(input('Digite o primeiro valor: '))
+segundo_valor = int(input('Digite o segundo valor: '))
+terceiro_valor = int(input('Digite o terceiro valor: '))
+resultado = primeiro_valor + segundo_valor + terceiro_valor
+
+print(f"O resultado da soma é {resultado}")
