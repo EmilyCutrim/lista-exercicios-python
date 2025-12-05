16ed5a191ab5f7b8f59309e7c15392ff07826704
EmilyCutrim <emilykerendev@gmail.com>
2025-12-04 22:26:24 -0300

    feat(code): Adiciona script para cálculo de média aritmética

---
+++ calculos/media_notas.py
@@ -0,0 +1,6 @@
+nota_um = float (input('Digite a primeira nota: '))
+nota_dois = float (input('Digite a segunda nota: '))
+nota_tres = float (input('Digite a terceira nota: '))
+resultado = (nota_um + nota_dois + nota_tres) / (3)
+
+print(f'Sua média é {resultado}!')
