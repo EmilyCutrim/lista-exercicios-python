4536dcf5174f4f812b25c7dc2543e311e88469bc
EmilyCutrim <emilykerendev@gmail.com>
2025-12-04 22:27:08 -0300

    feat(code): Adiciona validação condicional para percentual de crescimento

---
+++ condicional/percentual.py
@@ -0,0 +1,5 @@
+texto = int(input('Digite o porcentual de crescimento de produção da empresa: '))
+if texto > 0:
+  print(f'Porcentagem positiva {texto}%')
+else:
+  print(f'Porcentagem negativa {texto}%')
