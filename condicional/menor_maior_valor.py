c4008fc21e40dbb6806a92c3228363e15c5de2ef
EmilyCutrim <emilykerendev@gmail.com>
2025-12-04 22:26:48 -0300

    feat(code): Adiciona lógica para identificar o maior entre dois números

---
+++ condicional/menor_maior_valor.py
@@ -0,0 +1,8 @@
+primeiro_numero = int(input('Digite o primeiro número: '))
+segundo_numero = int(input('Digite o segundo número: '))
+
+if primeiro_numero > segundo_numero:
+  print(f'O número {primeiro_numero} é o maior.')
+
+else:
+  print(f'O número {segundo_numero} é o maior.')
